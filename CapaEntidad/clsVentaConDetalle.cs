362d8997da36929f8829264e6d8de5a1be7ce0be
joseiberico <josesito.x_x@hotmail.com>
2024-10-03 10:58:30 -0500

    clase adicional venta

---
+++ CapaEntidad/clsVentaConDetalle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class clsVentaConDetalle
+    {
+        public clsVenta objVentas { get; set; }
+        public clsListaDetalleVenta objDetalleVentas { get; set; }
+    }
+}
