289c214ac35ae632f1a63e25087f7bb3fb98e3c2
joseiberico <josesito.x_x@hotmail.com>
2024-10-03 11:10:03 -0500

    datos adicionales

--- CapaNegocio/obj/Debug/net6.0/CapaNegocio.AssemblyInfo.cs
+++ CapaNegocio/obj/Debug/net6.0/CapaNegocio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CapaNegocio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d222ced945a2955affce0727dff2c49eeae6e08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d660fcfe8626495fe44ed9bd7331612c3753936")]
 [assembly: System.Reflection.AssemblyProductAttribute("CapaNegocio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CapaNegocio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
